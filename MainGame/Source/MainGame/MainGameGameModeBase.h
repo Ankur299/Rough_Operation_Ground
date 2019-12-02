dc3617a7b6a46068a692839513218750da5524f7
Ankur299 <ankurjyoti299@gmail.com>
2019-12-02 09:40:42 +0530

    Project SetUp

---
+++ MainGame/Source/MainGame/MainGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MainGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAINGAME_API AMainGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
